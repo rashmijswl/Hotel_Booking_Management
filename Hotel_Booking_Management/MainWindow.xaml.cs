4190c54af4f613d1bab353f92c713cd8009651c7
rashmijswl <rashmijaiswal0316@gmail.com>
2021-08-22 13:21:43 -0400

    Login working. Created new Table for login: Admin1

--- Hotel_Booking_Management/MainWindow.xaml.cs
+++ Hotel_Booking_Management/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Hotel_Booking_Management
 {
@@ -27,7 +29,37 @@ namespace Hotel_Booking_Management
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-4K6QFE4\SQLEXPRESS;Initial Catalog=HotelBooking;Integrated Security=True");
+            try
+            {
+                if (sqlcon.State == ConnectionState.Closed)
+                {
+                    sqlcon.Open();
+                    SqlCommand cmd = new SqlCommand(@"SELECT * FROM [Admin1] WHERE UserName = @uname AND Password = @pwd", sqlcon);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@uname", tbusername.Text);
+                    cmd.Parameters.AddWithValue("@pwd", tbpassword.Text);
+                    int cnt = Convert.ToInt32(cmd.ExecuteScalar());
+                   
+                    if (cnt == 1)
+                    {
+                        Window1 wnd1 = new Window1();
+                        wnd1.Show();
+                        this.Close();
+                        //MessageBox.Show("Working");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect username or password");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong {0}", ex.Message.ToString());
 
+
+            }
         }
     }
 }
