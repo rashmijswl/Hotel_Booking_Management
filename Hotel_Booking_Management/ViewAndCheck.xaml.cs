64da91048f7de51b331805e6a5c9fad16bbc9a2d
rashmijswl <rashmijaiswal0316@gmail.com>
2021-08-22 23:47:39 -0400

    All Done. Validation left!

--- Hotel_Booking_Management/ViewAndCheck.xaml.cs
+++ Hotel_Booking_Management/ViewAndCheck.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Hotel_Booking_Management
 {
@@ -17,16 +19,59 @@ namespace Hotel_Booking_Management
     /// </summary>
     public partial class ViewAndCheck : Window
     {
+
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-4K6QFE4\SQLEXPRESS;Initial Catalog=HotelBooking;Integrated Security=True");
+
         public ViewAndCheck()
         {
             InitializeComponent();
+          
         }
+
 
         private void btnback_Click(object sender, RoutedEventArgs e)
         {
             Window1 wnd1 = new Window1();
             wnd1.Show();
             this.Close();
+        }
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+                LoadGrid();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        public void LoadGrid()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Bookings Where Booking_FromDate >= @Booking_FromDate AND Booking_ToDate <= @Booking_ToDate", sqlcon);
+
+            cmd.Parameters.AddWithValue("@Booking_FromDate", dpFromDate.SelectedDate);
+            cmd.Parameters.AddWithValue("@Booking_ToDate", dpToDate.SelectedDate);
+
+
+            DataTable dt = new DataTable();
+            sqlcon.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            sqlcon.Close();
+            dataGrid.ItemsSource = dt.DefaultView;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tbapartmentcount.Text = "100";
+            tbbeachviewcount.Text = "50";
+            tbpresidentavailcount.Text = "8";
         }
     }
 }
