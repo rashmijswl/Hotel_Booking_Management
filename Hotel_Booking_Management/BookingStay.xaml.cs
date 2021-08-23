ee751761c9390175839817a1f720946c85f26547
rashmijswl <rashmijaiswal0316@gmail.com>
2021-08-22 22:47:31 -0400

    Booking stay, Validation Pending

--- Hotel_Booking_Management/BookingStay.xaml.cs
+++ Hotel_Booking_Management/BookingStay.xaml.cs
@@ -26,29 +26,58 @@ namespace Hotel_Booking_Management
 
         private void btnBook_Click(object sender, RoutedEventArgs e)
         {
-            int roomtype = 0;
-            SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-4K6QFE4\SQLEXPRESS;Initial Catalog=HotelBooking;Integrated Security=True");
+            try
+            {
+                int roomid = 0;
+                string roomtype;
+                SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-4K6QFE4\SQLEXPRESS;Initial Catalog=HotelBooking;Integrated Security=True");
 
-            SqlCommand cmd = new SqlCommand("INSERT INTO StudentInfo (Customer_Name, Customer_Phone, Customer_Email, Customer_ID_type, Booking_FromDate, Booking_ToDate,  Payment_Card_number) VALUES (@Customer_Name, @Customer_Phone, @Customer_Email, @Customer_ID_type, @RoomID, @Booking_FromDate, @Booking_ToDate, @Payment_Card_number)", sqlcon);
-            cmd.CommandType = CommandType.Text;
+                SqlCommand cmd = new SqlCommand("INSERT INTO Bookings (Customer_Name, Customer_Phone, Customer_Email, Customer_ID_type, RoomID, Booking_FromDate, Booking_ToDate,  Payment_Card_number, RoomType) VALUES (@Customer_Name, @Customer_Phone, @Customer_Email, @Customer_ID_type, @RoomID, @Booking_FromDate, @Booking_ToDate, @Payment_Card_number,@RoomType)", sqlcon);
+                cmd.CommandType = CommandType.Text;
 
 
-            cmd.Parameters.AddWithValue("@Customer_Name", txtCustomerName.Text);
-            cmd.Parameters.AddWithValue("@Customer_Phone", txtPhone.Text);
-            cmd.Parameters.AddWithValue("@Customer_Email", txtEmailId1.Text);
-            cmd.Parameters.AddWithValue("@Customer_ID_type", txtCustomerIdType.Text);
-            cmd.Parameters.AddWithValue("@Booking_FromDate", dpFromDate.SelectedDate);
-            cmd.Parameters.AddWithValue("@Booking_ToDate", dbToDate.SelectedDate);
-            cmd.Parameters.AddWithValue("@Payment_Card_number", txtCreditCardNo.Text);
+                cmd.Parameters.AddWithValue("@Customer_Name", txtCustomerName.Text);
+                cmd.Parameters.AddWithValue("@Customer_Phone", txtPhone.Text);
+                cmd.Parameters.AddWithValue("@Customer_Email", txtEmailId1.Text);
+                cmd.Parameters.AddWithValue("@Customer_ID_type", txtCustomerIdType.Text);
+                if (rbPresidential.IsChecked == true)
+                {
+                    roomid = 1;
+                    roomtype = rbPresidential.Content.ToString();
+                }
+                    
+                else if (rbBeachView.IsChecked == true)
+                {
+                    roomid = 2;
+                    roomtype = rbBeachView.Content.ToString();
+                }
+                else
+                {
+                    roomid = 3;
+                    roomtype = rbApartment.Content.ToString();
+                }
 
-            if (rbPresidential.IsChecked == true)            
-                roomtype = 1;        
-            else if (rbBeachView.IsChecked == true)
-                roomtype = 2;
-            else
-                roomtype = 3;
+                cmd.Parameters.AddWithValue("@RoomID", roomid);
+                cmd.Parameters.AddWithValue("@Booking_FromDate", dpFromDate.SelectedDate);
+                cmd.Parameters.AddWithValue("@Booking_ToDate", dbToDate.SelectedDate);
+                cmd.Parameters.AddWithValue("@Payment_Card_number", txtCreditCardNo.Text);
+                cmd.Parameters.AddWithValue("@RoomType", roomtype);
 
-            cmd.Parameters.AddWithValue("@RoomID", roomtype);
+                sqlcon.Open();
+                cmd.ExecuteNonQuery();
+                sqlcon.Close();
+
+                MessageBox.Show("Stay booked!!");
+                Window1 wnd1 = new Window1();
+                wnd1.Show();
+                this.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: {0}", ex.Message);
+            }
         }
 
         private void btnback_Click(object sender, RoutedEventArgs e)
